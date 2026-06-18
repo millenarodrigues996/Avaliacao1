ddd84d26f7cd46513503b472c57c2fdfffee08f1
unknown <Inovape@CBRANB155827.ifb.local>
2026-06-18 17:18:28 -0300

    Sexto Exercicio

--- arquivo.js
+++ arquivo.js
@@ -18,5 +18,29 @@ switch (pontuação) {
 }
 console.log(`A categoria é ${categoria}`)
 
+//3. Sequência de Fibonacci: 
+
+//for (let i = 0; i <=34; i**) {
+
+//4. Simulador de Bateria: 
+//const nivelDaBateria = 100
+
+
+//5. Função para Calcular Dias de Vida:
+
+//6. Verificador de Aprovação Escolar: 
+const frequencia = 75
+let nota1 = 8
+let nota2 = 5
+let nota3 = 7
+let nota4 = 5
+const media = (nota1 + nota2 + nota3 + nota4 /4)
+if (media < 6 && frequencia < 75) {
+    console.log("Reprovado")
+} else if (media >= 6 && frequencia < 75) {
+    console.log("Reprovado")
+} else if (media >=6 && frequencia >= 75) {
+    console.log("Aprovado")
+}
 
 
